8ff9ba2f18881140858120d7cf05ef9ebc36659b
Gareth Brown <gaz@gbmail.me>
2020-04-22 18:01:17 +0100

    refactor

--- magic_words/__init__.py
+++ magic_words/__init__.py
@@ -3,7 +3,7 @@ from string import ascii_lowercase
 from simplecrypt import decrypt
 
 
-N = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+N = range(1, 26)
 SPECIAL = 18
 MEANINGFUL = 75
 SCORES = dict(zip(ascii_lowercase, N))
